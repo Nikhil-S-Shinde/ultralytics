d5b5ed0ed7b1e5626d7b8a26f0789db1b53536ac
Nikhil-S-Shinde <90606055+Nikhil-S-Shinde@users.noreply.github.com>
2025-02-09 02:55:18 +0530

    modification in _setup_ddp()
    changed dist.init_process_group( to use gloo if it detects KAGGLE
    
    Signed-off-by: Nikhil-S-Shinde <90606055+Nikhil-S-Shinde@users.noreply.github.com>

--- ultralytics/engine/trainer.py
+++ ultralytics/engine/trainer.py
@@ -220,8 +220,14 @@ class BaseTrainer:
         self.device = torch.device("cuda", RANK)
         # LOGGER.info(f'DDP info: RANK {RANK}, WORLD_SIZE {world_size}, DEVICE {self.device}')
         os.environ["TORCH_NCCL_BLOCKING_WAIT"] = "1"  # set to enforce timeout
+        # dist.init_process_group(
+        #     backend="nccl" if dist.is_nccl_available() else "gloo",
+        #     timeout=timedelta(seconds=10800),  # 3 hours
+        #     rank=RANK,
+        #     world_size=world_size,
+        # )
         dist.init_process_group(
-            backend="nccl" if dist.is_nccl_available() else "gloo",
+            backend="gloo" if os.environ.get("KAGGLE_KERNEL_RUN_TYPE") else "ddp_notebook",
             timeout=timedelta(seconds=10800),  # 3 hours
             rank=RANK,
             world_size=world_size,
