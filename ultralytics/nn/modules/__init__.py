49c50901416ffd5065735960eef40e60aec80fa2
what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>
2025-02-21 23:10:46 +0530

    Update __init__.py
    Signed-off-by: what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>

--- ultralytics/nn/modules/__init__.py
+++ ultralytics/nn/modules/__init__.py
@@ -86,6 +86,7 @@ from .transformer import (
     TransformerBlock,
     TransformerEncoderLayer,
     TransformerLayer,
+    EMA.
 )
 
 __all__ = (
@@ -160,4 +161,5 @@ __all__ = (
     "PSA",
     "TorchVision",
     "Index",
+    "EMA",
 )
