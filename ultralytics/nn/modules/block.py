3d1c2b14834f3f92b00a0f96f29f150a40f4b804
what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>
2025-03-02 03:40:35 +0530

    Update block.py
    Signed-off-by: what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -309,6 +309,22 @@ class C2fGhost(nn.Module):
         y.extend(m(y[-1]) for m in self.m)
         return self.cv2(torch.cat(y, 1))
 
+class C3(nn.Module):
+    """CSP Bottleneck with 3 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initialize the CSP Bottleneck with given channels, number, shortcut, groups, and expansion values."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=((1, 1), (3, 3)), e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck with 2 convolutions."""
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
 class C3x(C3):
     """C3 module with cross-convolutions."""
 
