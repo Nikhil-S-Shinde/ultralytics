b3a58a087264f1309090fb5ec4fd330cd64ab41b
what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>
2025-03-02 16:38:32 +0530

    Update tasks.py
    Signed-off-by: what_ <90606055+Nikhil-S-Shinde@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1022,7 +1022,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             C2PSA,
             BiFPN_Concat2,
             BiFPN_Concat3,
-            # DepthwiseConvBlock,
+            DWC3k2,
         }
     )
     for i, (f, n, m, args) in enumerate(d["backbone"] + d["head"]):  # from, number, module, args
